884c1278dbff419a371925708e45915f2d09b703
pqoqubbw <pqoqubbw@gmail.com>
2024-10-31 23:41:47 +0100

    add og

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,8 @@ import { Header } from '@/components/header';
 import { CommandMenu } from '@/components/comman-menu';
 import { Footer } from '@/components/footer';
 
+import ogImage from './og.png';
+
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
   variable: '--font-geist-sans',
@@ -19,8 +21,67 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-  title: 'pqoqubbw/icons',
+  metadataBase: new URL('https://icons.pqoqubbw.dev'),
+  openGraph: {
+    title: 'pqoqubbw/icons',
+    description: 'beautifully crafted animated icons',
+    siteName: 'pqoqubbw/icons',
+    type: 'website',
+    locale: 'en_US',
+    url: 'https://icons.pqoqubbw.dev',
+    images: [
+      {
+        url: ogImage.src,
+        width: ogImage.width,
+        height: ogImage.height,
+      },
+    ],
+  },
+  robots: {
+    index: true,
+    follow: true,
+    'max-image-preview': 'large',
+    'max-snippet': -1,
+    'max-video-preview': -1,
+    googleBot: 'index, follow',
+  },
+  applicationName: 'pqoqubbw/icons',
+  appleWebApp: {
+    title: 'pqoqubbw/icons',
+    statusBarStyle: 'default',
+    capable: true,
+  },
+  title: {
+    default: 'pqoqubbw/icons',
+    template: `%s - pqoqubbw/icons`,
+  },
   description: 'beautifully crafted animated icons',
+  twitter: {
+    card: 'summary_large_image',
+    title: 'pqoqubbw/icons',
+    description: 'beautifully crafted animated icons',
+    images: [
+      {
+        url: ogImage.src,
+        width: ogImage.width,
+        height: ogImage.height,
+      },
+    ],
+  },
+  icons: {
+    icon: [
+      {
+        url: '/favicon.ico',
+        type: 'image/x-icon',
+      },
+    ],
+    shortcut: [
+      {
+        url: '/favicon.ico',
+        type: 'image/x-icon',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
