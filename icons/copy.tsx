0b256ca83b8d7fa264fbc0ee25ba7c41ea7485b9
pqoqubbw <pqoqubbw@gmail.com>
2024-11-02 19:59:58 +0100

    fix copy icon

--- icons/copy.tsx
+++ icons/copy.tsx
@@ -1,6 +1,13 @@
 'use client';
 
-import { motion, useAnimation } from 'framer-motion';
+import { motion, Transition, useAnimation } from 'framer-motion';
+
+const defaultTransition: Transition = {
+  type: 'spring',
+  stiffness: 160,
+  damping: 17,
+  mass: 1,
+};
 
 const CopyIcon = () => {
   const controls = useAnimation();
@@ -22,14 +29,27 @@ const CopyIcon = () => {
         strokeLinecap="round"
         strokeLinejoin="round"
       >
-        <rect width="14" height="14" x="8" y="8" rx="2" ry="2" />
+        <motion.rect
+          width="14"
+          height="14"
+          x="8"
+          y="8"
+          rx="2"
+          ry="2"
+          variants={{
+            normal: { translateY: 0, translateX: 0 },
+            animate: { translateY: -3, translateX: -3 },
+          }}
+          animate={controls}
+          transition={defaultTransition}
+        />
         <motion.path
           d="M4 16c-1.1 0-2-.9-2-2V4c0-1.1.9-2 2-2h10c1.1 0 2 .9 2 2"
           variants={{
             normal: { x: 0, y: 0 },
-            animate: { x: 6, y: 6 },
+            animate: { x: 3, y: 3 },
           }}
-          transition={{ type: 'spring', stiffness: 140, damping: 17, mass: 1 }}
+          transition={defaultTransition}
           animate={controls}
         />
       </svg>
