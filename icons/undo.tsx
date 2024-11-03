0c801112bac229d54ce402655951eef651b87d68
pqoqubbw <pqoqubbw@gmail.com>
2024-11-03 23:54:35 +0100

    update undo animation

--- icons/undo.tsx
+++ icons/undo.tsx
@@ -1,28 +1,8 @@
-"use client";
+'use client';
 
-import { motion, useAnimation, Variants } from "framer-motion";
+import { cubicBezier, motion, useAnimation } from 'framer-motion';
 
-const pathVariants: Variants = {
-  normal: {
-    rotate: 0,
-    opacity: 1,
-  },
-  animate: {
-    opacity: [0, 1],
-    d: ["M4 7v6h6", "M3 7v6h6"],
-    rotate: [15, 0],
-    transition: { duration: 0.4 },
-  },
-};
-
-const secondPathVariants: Variants = {
-  normal: { pathLength: 1, opacity: 1, pathOffset: 0 },
-  animate: {
-    pathLength: [0, 1],
-    opacity: [0, 1],
-    pathOffset: [1, 0],
-  },
-};
+const customEasing = cubicBezier(0.25, 0.1, 0.25, 1);
 
 const Undo = () => {
   const controls = useAnimation();
@@ -30,8 +10,8 @@ const Undo = () => {
   return (
     <div
       className="cursor-pointer select-none p-2 hover:bg-accent rounded-md transition-colors duration-200 flex items-center justify-center"
-      onMouseEnter={() => controls.start("animate")}
-      onMouseLeave={() => controls.start("normal")}
+      onMouseEnter={() => controls.start('animate')}
+      onMouseLeave={() => controls.start('normal')}
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
@@ -44,10 +24,31 @@ const Undo = () => {
         strokeLinecap="round"
         strokeLinejoin="round"
       >
-        <motion.path variants={pathVariants} d="M3 7v6h6" animate={controls} />
         <motion.path
-          transition={{ duration: 0.4, delay: 0.2 }}
-          variants={secondPathVariants}
+          transition={{
+            duration: 0.6,
+            ease: customEasing,
+          }}
+          variants={{
+            normal: { translateX: 0, translateY: 0, rotate: 0 },
+            animate: {
+              translateX: [0, 2.1, 0],
+              translateY: [0, -1.4, 0],
+              rotate: [0, 12, 0],
+            },
+          }}
+          animate={controls}
+          d="M3 7v6h6"
+        />
+        <motion.path
+          transition={{
+            duration: 0.6,
+            ease: customEasing,
+          }}
+          variants={{
+            normal: { pathLength: 1 },
+            animate: { pathLength: [1, 0.8, 1] },
+          }}
           animate={controls}
           d="M21 17a9 9 0 0 0-9-9 9 9 0 0 0-6 2.3L3 13"
         />
