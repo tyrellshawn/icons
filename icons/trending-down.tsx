24f3df889aaead7223b81e5c0c8a218a48f3fae6
killian-dv <killian.david@etudiant.univ-reims.fr>
2024-11-07 23:27:26 +0100

    feat: add some bounce

--- icons/trending-down.tsx
+++ icons/trending-down.tsx
@@ -1,6 +1,62 @@
 'use client';
 
+import type { Variants } from 'framer-motion';
 import { motion, useAnimation } from 'framer-motion';
+
+const svgVariants: Variants = {
+  animate: {
+    x: 0,
+    y: 0,
+    translateX: [0, 2, 0],
+    translateY: [0, 2, 0],
+    transition: {
+      duration: 0.3,
+      delay: 0.4,
+    },
+  },
+};
+
+const pathVariants: Variants = {
+  normal: {
+    opacity: 1,
+    pathLength: 1,
+    transition: {
+      duration: 0.4,
+      opacity: { duration: 0.1 },
+    },
+  },
+  animate: {
+    opacity: [0, 1],
+    pathLength: [0, 1],
+    pathOffset: [1, 0],
+    transition: {
+      duration: 0.4,
+      opacity: { duration: 0.1 },
+    },
+  },
+};
+
+const arrowVariants: Variants = {
+  normal: {
+    opacity: 1,
+    pathLength: 1,
+    transition: {
+      delay: 0.3,
+      duration: 0.3,
+      opacity: { duration: 0.1, delay: 0.3 },
+    },
+  },
+  animate: {
+    opacity: [0, 1],
+    pathLength: [0, 1],
+    pathOffset: [0.5, 0],
+    transition: {
+      delay: 0.3,
+      duration: 0.3,
+      opacity: { duration: 0.1, delay: 0.3 },
+    },
+  },
+};
 
 const TrendingDownIcon = () => {
   const controls = useAnimation();
@@ -11,7 +67,7 @@ const TrendingDownIcon = () => {
       onMouseEnter={() => controls.start('animate')}
       onMouseLeave={() => controls.start('normal')}
     >
-      <svg
+      <motion.svg
         xmlns="http://www.w3.org/2000/svg"
         width="28"
         height="28"
@@ -21,56 +77,23 @@ const TrendingDownIcon = () => {
         strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
+        variants={svgVariants}
+        initial="normal"
+        animate={controls}
       >
         <motion.polyline
           points="22 17 13.5 8.5 8.5 13.5 2 7"
-          variants={{
-            normal: {
-              opacity: 1,
-              pathLength: 1,
-              transition: {
-                duration: 0.4,
-                opacity: { duration: 0.1 },
-              },
-            },
-            animate: {
-              opacity: [0, 1],
-              pathLength: [0, 1],
-              pathOffset: [1, 0],
-              transition: {
-                duration: 0.4,
-                opacity: { duration: 0.1 },
-              },
-            },
-          }}
+          variants={pathVariants}
+          initial="normal"
           animate={controls}
         />
         <motion.polyline
           points="16 17 22 17 22 11"
-          variants={{
-            normal: {
-              opacity: 1,
-              pathLength: 1,
-              transition: {
-                delay: 0.3,
-                duration: 0.3,
-                opacity: { duration: 0.1, delay: 0.3 },
-              },
-            },
-            animate: {
-              opacity: [0, 1],
-              pathLength: [0, 1],
-              pathOffset: [0.5, 0],
-              transition: {
-                delay: 0.3,
-                duration: 0.3,
-                opacity: { duration: 0.1, delay: 0.3 },
-              },
-            },
-          }}
+          variants={arrowVariants}
+          initial="normal"
           animate={controls}
         />
-      </svg>
+      </motion.svg>
     </div>
   );
 };
