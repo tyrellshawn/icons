d3d83a2d574afe4d1c07806a75bb022338386281
pqoqubbw <pqoqubbw@gmail.com>
2024-11-13 19:27:34 +0100

    fix airplane icon animation

--- icons/airplane.tsx
+++ icons/airplane.tsx
@@ -2,17 +2,13 @@
 
 import { motion, useAnimation } from 'framer-motion';
 
-const iconSize = 28;
-const viewBox = '-6 0 34 30';
-const strokeWidth = 2;
-
-const speedLines = [
+const SPEED_LINES = [
   { x1: -6, y1: 26, x2: 0, y2: 20, delay: 0.1 },
   { x1: -4, y1: 28, x2: 2, y2: 22, delay: 0.2 },
   { x1: -2, y1: 30, x2: 4, y2: 24, delay: 0.3 },
 ];
 
-export function AirplaneIcon() {
+const AirplaneIcon = () => {
   const controls = useAnimation();
 
   return (
@@ -23,37 +19,32 @@ export function AirplaneIcon() {
     >
       <motion.svg
         xmlns="http://www.w3.org/2000/svg"
-        width={iconSize}
-        height={iconSize}
-        viewBox={viewBox}
+        width="28"
+        height="28"
+        viewBox="-6 0 34 30"
         fill="none"
         stroke="currentColor"
-        strokeWidth={strokeWidth}
+        strokeWidth="2"
         strokeLinecap="round"
         strokeLinejoin="round"
         animate={controls}
+        transition={{
+          duration: 0.5,
+          times: [0, 0.6, 1],
+          type: 'spring',
+          stiffness: 200,
+          damping: 10,
+        }}
         variants={{
           initial: { x: 0, y: 0 },
           hover: {
-            x: [0, 14, 12],
-            y: [0, -14, -12],
-            transition: {
-              duration: 0.5,
-              times: [0, 0.6, 1],
-              type: 'spring',
-              stiffness: 200,
-              damping: 10,
-            },
+            x: [0, 5, 3],
+            y: [0, -5, -3],
           },
         }}
       >
-        <motion.path
-          d="M17.8 19.2L16 11l3.5-3.5C21 6 21.5 4 21 3c-1-.5-3 0-4.5 1.5L13 8 4.8 6.2c-.5-.1-.9.1-1.1.5l-.3.5c-.2.5-.1 1 .3 1.3L9 12l-2 3H4l-1 1 3 2 2 3 1-1v-3l3-2 3.5 5.3c.3.4.8.5 1.3.3l.5-.2c.4-.3.6-.7.5-1.2z"
-          initial={{ pathLength: 0 }}
-          animate={{ pathLength: 1 }}
-          transition={{ duration: 1.5, ease: 'easeInOut' }}
-        />
-        {speedLines.map((line, index) => (
+        <path d="M17.8 19.2L16 11l3.5-3.5C21 6 21.5 4 21 3c-1-.5-3 0-4.5 1.5L13 8 4.8 6.2c-.5-.1-.9.1-1.1.5l-.3.5c-.2.5-.1 1 .3 1.3L9 12l-2 3H4l-1 1 3 2 2 3 1-1v-3l3-2 3.5 5.3c.3.4.8.5 1.3.3l.5-.2c.4-.3.6-.7.5-1.2z" />
+        {SPEED_LINES.map((line, index) => (
           <motion.line
             key={index}
             x1={line.x1}
@@ -73,4 +64,6 @@ export function AirplaneIcon() {
       </motion.svg>
     </div>
   );
-}
+};
+
+export { AirplaneIcon };
