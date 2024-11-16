5c5b5e5e1f89deb4708ed9d3206afaa333e14f37
pqoqubbw <pqoqubbw@gmail.com>
2024-11-16 01:38:58 +0100

    fix ban icon

--- icons/ban.tsx
+++ icons/ban.tsx
@@ -60,9 +60,9 @@ const BanIcon = () => {
         viewBox="0 0 24 24"
         fill="none"
         stroke="currentColor"
-        stroke-width="2"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       >
         <motion.circle
           cx="12"
