94efc52a2e6b55b46bb22b8583d4529049e1357e
pqoqubbw <pqoqubbw@gmail.com>
2024-12-01 03:19:18 +0100

    fix snowfall glitch on first render

--- components/snowfall.tsx
+++ components/snowfall.tsx
@@ -1,8 +1,21 @@
 'use client';
 
+import { useEffect, useState } from 'react';
 import Snowfall from 'react-snowfall';
 
 const SnowfallComponent = () => {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsVisible(true);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (!isVisible) return null;
+
   return (
     <Snowfall
       snowflakeCount={150}
